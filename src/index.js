2a6fe463a502993cb89b969a01c959977ec0cc51
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-02-18 17:44:22 +0530

    added eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./styles/index.scss";
 ReactDOM.render(
     <h1>Hello World!</h1>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 if(module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
