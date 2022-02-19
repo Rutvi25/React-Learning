484350fbfe868683fa50992ec0ad75b3613e5180
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-02-19 17:55:53 +0530

    checkbox functionality added

--- React-Test/color-button/src/App.test.js
+++ React-Test/color-button/src/App.test.js
@@ -14,3 +14,21 @@ test('button has correct initial color', () => {
   // expects the button text to be 'Change to red'
   expect(colorButton.textContent).toBe('Change to red')
 });
+test('initial conditions', () => {
+  render(<App />);
+  // check the button starts out enabled
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
+  expect(colorButton).toBeEnabled();
+  // check the checkbox starts out unchecked}
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+})
+test('checkbox makes button disabled and enabled on alternative clicks', () => {
+  render(<App />);
+  const checkbox = screen.getByRole('checkbox');
+  const button = screen.getByRole('button');
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+});
