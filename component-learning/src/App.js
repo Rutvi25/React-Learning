c3aa33c7d00c8d95c85896217ce55d341e6ae72e
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-02-23 10:39:40 +0530

    Class Component

--- component-learning/src/App.js
+++ component-learning/src/App.js
@@ -1,9 +1,11 @@
 import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Greet name="John"/>
+      <Welcome name="John"/>
     </div>
   );
 }
