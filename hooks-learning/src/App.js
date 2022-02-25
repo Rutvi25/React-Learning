cf3e4b97349811fec489008d822b6087522c1ccc
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-02-25 16:10:04 +0530

    useState with object

--- hooks-learning/src/App.js
+++ hooks-learning/src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
       <ClassCounter /><br />
-      <HookCounter />
+      <HookCounter /><br />
+      <HookCounterTwo />
+      <br />
+      <HookCounterThree />
     </div>
   );
 }
