a2eb04a2c15d84ddec1cdef9522f53f15b3578a6
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-03-08 17:11:26 +0530

    react-redux, action, reducer, store, connect

--- react-redux-learning/src/Components/CakeContainer.js
+++ react-redux-learning/src/Components/CakeContainer.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { buyCake } from '../redux'
 
-function CakeContainer() {
+function CakeContainer(props) {
   return (
     <div>
-      <h2>Number of Cakes</h2>
-      <button>Buy Cakes</button>
+      <h2>Number of Cakes : {props.totalCakes}</h2>
+      <button onClick={props.buyCake}>Buy Cakes</button>
     </div>
   )
 }
+const mapStateToProps = state => {
+  return {
+    totalCakes: state.totalCakes
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    buyCake: () => dispatch(buyCake())
+  }
+}
 
-export default CakeContainer
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(CakeContainer)
---
+++ react-redux-learning/src/redux/cakes/cakeReducer.js
@@ -0,0 +1,14 @@
+import { BUY_CAKE } from "./cakeTypes"
+const initialState = {
+  totalCakes: 10
+}
+const cakeReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case BUY_CAKE: return {
+      ...state, 
+      totalCakes: state.totalCakes - 1
+    }
+    default: return state
+  }
+}
+export default cakeReducer
