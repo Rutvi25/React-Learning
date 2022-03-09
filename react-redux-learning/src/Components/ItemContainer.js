8b5e69a6415dab22e2499b361f31718117829639
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-03-09 17:56:29 +0530

    mapDispatchToProps

--- react-redux-learning/src/Components/ItemContainer.js
+++ react-redux-learning/src/Components/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIceCream } from '../redux'
 
 function ItemContainer(props) {
   return (
     <div>
       <h2>Item : {props.item}</h2>
+      <button onClick={props.buyItem}>Buy Items</button>
     </div>
   )
 }
@@ -16,6 +18,13 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake ? () => dispatch(buyCake()) : () => dispatch(buyIceCream())
+
+  return {
+    buyItem : dispatchFunction
+  }
+}
 
 
-export default connect (mapStateToProps)(ItemContainer)
+export default connect (mapStateToProps, mapDispatchToProps)(ItemContainer)
