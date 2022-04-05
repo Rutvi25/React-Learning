823e17066f6cf1036d267d8a4b4a91136005ed32
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-04-05 16:55:15 +0530

    basic props

---
+++ react-typescript/src/components/Greet.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+type GreetProps ={
+  name: string
+  messageCount: number 
+  isLoggedIn: boolean
+}
+function Greet(props: GreetProps) {
+  return (
+    <div>
+      <h2>
+        {
+          props.isLoggedIn 
+          ? `Hello, {props.name}! You have {props.messageCount} messages` 
+          : `Welcome User`
+        }      
+      </h2>
+    </div>
+  )
+}
+
+export default Greet
---
+++ react-typescript/src/components/PersonList.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+type PersonListProps = {
+  names: {
+    first: string
+    last: string
+  }[]
+}
+
+function PersonList(props: PersonListProps) {
+  return (
+    <div>
+      {props.names.map((name) => {
+        return (
+          <h3 key={name.first}>{name.first} {name.last}</h3>
+        )
+      })}
+    </div>
+  )
+}
+
+export default PersonList
