424999e9eb1be4cbb5d0bd972ba1f2f218e34c16
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-04-06 11:19:29 +0530

    event props

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -5,6 +5,8 @@ import PersonList from './components/PersonList';
 import Status from './components/Status';
 import Heading from './components/Heading';
 import Oscar from './components/Oscar';
+import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
   const personName = {
@@ -35,6 +37,10 @@ function App() {
       <Oscar>
         <Heading>Heading</Heading>
       </Oscar>
+      <br />
+      <Button handleClick={ (event, id) => console.log('Button Clicked!', event, id) } />
+      <br /><br />
+      <Input value='' handleChange={(event) => console.log(event)}/>
     </div>
   );
 }
